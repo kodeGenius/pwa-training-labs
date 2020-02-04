e0781cb308fa39b7c9e8db0eb864ec625e9a1b89
Saheed Oladosu <oladosu777@gmail.com>
2020-02-04 13:08:19 +0100

    ne

--- fetch-api-lab/app/js/main.js
+++ fetch-api-lab/app/js/main.js
@@ -14,6 +14,36 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+function validateResponse(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+function readResponseAsJSON(response) {
+  return response.json();
+}
+
+function showImage(responseAsBlob) {
+  const container = document.getElementById('img-container');
+  const imgElem = document.createElement('img');
+  container.appendChild(imgElem);
+  const imgUrl = URL.createObjectURL(responseAsBlob);
+  imgElem.src = imgUrl;
+}
+
+function readResponseAsBlob(response) {
+  return response.blob();
+}
+
+function showText(responseAsText) {
+  const message = document.getElementById('message');
+  message.textContent = responseAsText;
+}
+
+const readResponseAsText = response => response.text();
+
 // helper functions ----------
 
 function logResult(result) {
@@ -28,8 +58,13 @@ function logError(error) {
 // Fetch JSON ----------
 
 function fetchJSON() {
-  // TODO
+  fetch('examples/animals.json')
+  .then(validateResponse)
+  .then(readResponseAsJSON)
+  .then(logResult)
+  .catch(logError);// TODO
 }
+
 const jsonButton = document.getElementById('json-btn');
 jsonButton.addEventListener('click', fetchJSON);
 
@@ -37,8 +72,13 @@ jsonButton.addEventListener('click', fetchJSON);
 // Fetch Image ----------
 
 function fetchImage() {
-  // TODO
+  fetch('examples/fetching.jpg')
+    .then(validateResponse)
+    .then(readResponseAsBlob)
+    .then(showImage)
+    .catch(logError)// TODO
 }
+
 const imgButton = document.getElementById('img-btn');
 imgButton.addEventListener('click', fetchImage);
 
@@ -46,6 +86,11 @@ imgButton.addEventListener('click', fetchImage);
 // Fetch text ----------
 
 function fetchText() {
+  fetch('examples/words.txt')
+  .then(validateResponse)
+  .then(readResponseAsText)
+  .then(showText)
+  .catch(logError)
   // TODO
 }
 const textButton = document.getElementById('text-btn');
@@ -55,7 +100,14 @@ textButton.addEventListener('click', fetchText);
 // HEAD request ----------
 
 function headRequest() {
-  // TODO
+  fetch('examples/words.txt', {
+    method: 'HEAD',
+    headers:'content-length'
+  })
+  .then(validateResponse)
+  .then(readResponseAsText)
+  .then(logResult)
+  .catch(logError);// TODO
 }
 const headButton = document.getElementById('head-btn');
 headButton.addEventListener('click', headRequest);
@@ -65,7 +117,17 @@ headButton.addEventListener('click', headRequest);
 
 /* NOTE: Never send unencrypted user credentials in production! */
 function postRequest() {
-  // TODO
+  const formData = new FormData(document.getElementById('msg-form'));
+  fetch('http://localhost:5000', {
+    method: 'POST',
+    body: formData,
+    //mode:"no-cors"
+  })
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(showText)
+    //.then(logResult)
+    .catch(logError);// TODO
 }
 const postButton = document.getElementById('post-btn');
 postButton.addEventListener('click', postRequest);
