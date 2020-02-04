f251c7ba493a80e02c2068c304321ce37fec39a7
Saheed Oladosu <oladosu777@gmail.com>
2020-02-04 13:02:37 +0100

    new commit

---
+++ service-worker-lab/app/service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+    // Add a call to skipWaiting here
+    self.skipWaiting();
+  });
+
+  // I'm a new service worker
+  
+  self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
+  });
+
+  self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+});
